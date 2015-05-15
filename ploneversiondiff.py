78493da98aea77266a60d07bd1b8ca42b23e24a1
Sebastian Jordan <jordanse@hu-berlin.de>
2015-05-15 19:41:46 +0200

    Add version to show all changes

--- ploneversiondiff.py
+++ ploneversiondiff.py
@@ -3,6 +3,7 @@
 import ConfigParser
 import argparse
 
+show_all = False
 
 def getDict(config):
     """extract versions from a config into a dictionary"""
@@ -20,12 +21,27 @@ def printDiffs(d1, d2):
             if d1[key] != d2[key]:
                 print("%s\n\t%s\t%s" % (key, d1[key], d2[key]))
 
+def printNotMatching((d1,filename1), (d2,filename2)):
+    """print versions set in only one of the files"""
+    print("Versions set in %s, but not set in %s" % (filename1,filename2))
+    for key in d1:
+        if not (key in d2):
+            print("%s: %s" % (key, d1[key]))
+    print("")
+    print("Versions set in %s, but not set in %s" % (filename2,filename1))
+    for key in d2:
+        if not (key in d1):
+            print("%s: %s" % (key, d2[key]))
+
 
 argp = argparse.ArgumentParser(description='Determin differences between plone version settings')
 argp.add_argument('filename1', metavar='FILE1',
                   help='filename of the first config file to read')
 argp.add_argument('filename2', metavar='FILE2',
                   help='filename of the second config file to read')
+argp.add_argument('-a', '--show-all',default=False, action='store_true',
+                  help='When passed, the program will show all differences, '
+                       'not only those of versions set in both files.')
 args=argp.parse_args()
 
 conf1 = ConfigParser.ConfigParser()
@@ -37,3 +53,7 @@ conf2.read(args.filename2)
 versions2 = getDict(conf2)
 
 printDiffs(versions1, versions2)
+
+if args.show_all:
+    print("")
+    printNotMatching((versions1, args.filename1), (versions2, args.filename2))
