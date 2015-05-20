8df539bb04d8ff0672e6d007fc38e51e63d067a6
Sebastian Jordan <jordanse@hu-berlin.de>
2015-05-20 16:34:31 +0200

    Implement flag to show only newer versions

--- ploneversiondiff.py
+++ ploneversiondiff.py
@@ -2,6 +2,7 @@
 
 import ConfigParser
 import argparse
+import distutils.version
 
 show_all = False
 
@@ -14,12 +15,20 @@ def getDict(config):
     return d
 
 
-def printDiffs(d1, d2):
+def printDiffs(d1, d2, only_newer=False):
     """print differing items of dictionaries to stdout"""
     for key in d1:
         if key in d2:
-            if d1[key] != d2[key]:
-                print("%s\n\t%s\t%s" % (key, d1[key], d2[key]))
+            vstring1 = d1[key]
+            v1 = distutils.version.LooseVersion(vstring1)
+            vstring2 = d2[key]
+            v2 = distutils.version.LooseVersion(vstring2)
+            # print the version difference, don't print the version
+            # difference if only_newer is activated and the version of
+            # the second dict is smaller (older) then the version of
+            # the first dict
+            if ((v1 != v2) and ((not only_newer) or (v1 < v2))):
+                print("%s\n\t%s\t%s" % (key, vstring1, vstring2))
 
 def printNotMatching((d1,filename1), (d2,filename2)):
     """print versions set in only one of the files"""
@@ -34,7 +43,8 @@ def printNotMatching((d1,filename1), (d2,filename2)):
             print("%s: %s" % (key, d2[key]))
 
 
-argp = argparse.ArgumentParser(description='Determin differences between plone version settings')
+argp = argparse.ArgumentParser(\
+    description='Determin differences between plone version settings')
 argp.add_argument('filename1', metavar='FILE1',
                   help='filename of the first config file to read')
 argp.add_argument('filename2', metavar='FILE2',
@@ -42,6 +52,11 @@ argp.add_argument('filename2', metavar='FILE2',
 argp.add_argument('-a', '--show-all',default=False, action='store_true',
                   help='When passed, the program will show all differences, '
                        'not only those of versions set in both files.')
+argp.add_argument('-n', '--only-newer', default=False, action='store_true',
+                  help='When passed, the program will only show differences '
+                  'when the version of FILE2 is higher than the version of '
+                  'FILE1')
+
 args=argp.parse_args()
 
 conf1 = ConfigParser.ConfigParser()
@@ -52,7 +67,7 @@ conf2 = ConfigParser.ConfigParser()
 conf2.read(args.filename2)
 versions2 = getDict(conf2)
 
-printDiffs(versions1, versions2)
+printDiffs(versions1, versions2, only_newer=args.only_newer)
 
 if args.show_all:
     print("")
